36025f8c9c9e8bbdb11a00f1fa0657e078ed1dc2
chitwan101e-ship-it <you@example.com>
2026-09-21 19:55:07 -0500

    Update Site source

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import type {MetadataRoute} from 'next';
 import {origin} from './seo';
 import {articles} from './stories';
-export default function sitemap():MetadataRoute.Sitemap{return ['','/game-worlds','/blog','/contact',...articles.map(a=>'/journal/'+a.slug)].map(path=>({url:origin+path}))}
+export default function sitemap():MetadataRoute.Sitemap{return ['','/about','/game-worlds','/blog','/contact',...articles.map(a=>'/journal/'+a.slug)].map(path=>({url:origin+path}))}
 
